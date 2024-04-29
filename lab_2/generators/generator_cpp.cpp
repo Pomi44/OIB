41be618ebc71c58c8fc2348856cefae94326ab52
KsushaK <ksushak1803@gmail.com>
2024-04-29 17:31:34 +0400

    complete all tasks

--- lab_2/generators/generator_cpp.cpp
+++ lab_2/generators/generator_cpp.cpp
@@ -2,7 +2,11 @@
 #include <random>
 #include <sstream>
 
-
+/**
+ * \brief Generates a random binary sequence of the specified size.
+ * \param size The size of the sequence to generate.
+ * \return A string representing the random binary sequence.
+ */
 std::string generateRandomBinarySequence(int size) {
     std::random_device rd;
     std::mt19937 gen(rd()); 
@@ -10,6 +14,7 @@ std::string generateRandomBinarySequence(int size) {
 
     std::stringstream ss;
 
+    // Generate random binary numbers and append them to the string stream.
     for (int i = 0; i < size; ++i) {
         ss << dis(gen);
     }
@@ -17,14 +22,22 @@ std::string generateRandomBinarySequence(int size) {
     return ss.str(); 
 }
 
+/**
+ * \brief Main function of the program.
+ * \return Return code.
+ */
 int main() {
     int size;
-    std::cout << "Введите размер последовательности: ";
+
+    // Request the size of the sequence from the user.
+    std::cout << "Enter the size of the sequence: ";
     std::cin >> size;
 
+    // Generate a random binary sequence.
     std::string randomBinarySequence = generateRandomBinarySequence(size);
 
-    std::cout << "Случайная последовательность бинарных чисел: " << randomBinarySequence << std::endl;
+    // Output the generated sequence to the screen.
+    std::cout << "Random binary sequence: " << randomBinarySequence << std::endl;
 
     return 0;
 }
--- lab_2/generators/generator_java.java
+++ lab_2/generators/generator_java.java
@@ -1,24 +1,38 @@
 import java.util.Random;
 
+/**
+ * A class to generate random binary sequences.
+ */
 public class RandomBinarySequenceGenerator {
 
-    // Функция для генерации случайной последовательности бинарных чисел
+    /**
+     * Generates a random binary sequence of the specified size.
+     *
+     * @param size The size of the sequence to generate.
+     * @return A string representing the random binary sequence.
+     */
     public static String generateRandomBinarySequence(int size) {
         Random random = new Random();
         StringBuilder binarySequence = new StringBuilder();
 
+        // Generate random binary numbers and append them to the sequence.
         for (int i = 0; i < size; ++i) {
-            int randomNumber = random.nextInt(2); // Генерация случайного числа 0 или 1
-            binarySequence.append(randomNumber); // Добавление случайного числа к последовательности
+            int randomNumber = random.nextInt(2); // Generate a random number 0 or 1
+            binarySequence.append(randomNumber); // Add the random number to the sequence
         }
 
         return binarySequence.toString();
     }
 
+    /**
+     * The main method of the program.
+     *
+     * @param args Command-line arguments (not used in this program).
+     */
     public static void main(String[] args) {
-        int size = 10; // Заданный размер последовательности
+        int size = 128; // The specified size of the sequence
         String randomBinarySequence = generateRandomBinarySequence(size);
 
-        System.out.println("Случайная последовательность бинарных чисел: " + randomBinarySequence);
+        System.out.println("Random binary sequence: " + randomBinarySequence);
     }
 }
